fe530cf7ea07979c63133e6b7fa42ad5d26d2774
Raj <alphaHL@dr.genetic>
2013-10-09 16:18:09 -0700

    first commit to git

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,33 @@ class StaticPagesController < ApplicationController
     @authors = Author.all
     @topics = [['politics', 'politics'],['coding', 'coding'],['science', 'science'],['stories', 'stories'],['music', 'music'],['books', 'books']]
   end
+  def science
+    @articles = Article.all
+    @authors = Author.all
+  end
+  def art
+    @articles = Article.all
+    @authors = Author.all
+  end
+  def politics
+    @articles = Article.all
+    @authors = Author.all
+  end
+  def coding
+    @articles = Article.all
+    @authors = Author.all
+  end
+  def books
+    @articles = Article.all
+    @authors = Author.all
+  end
+  def stories
+    @articles = Article.all
+    @authors = Author.all
+  end
+  def music
+    @articles = Article.all
+    @authors = Author.all
+  end
 end
+
